93dab87f7d6fc08ecd1356cb39881307380eecc9
Sam Landen <samantha.landen@joinpropel.com>
2025-08-28 12:40:36 -0400

    convert arrays to raw before merging
    this should fix an issue we are seeing with localForage being unable to
    save the store.

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,11 @@
 import deepmerge from 'deepmerge'
+import { toRaw } from 'vue'
 
 export type MergeOptionType = 'replaceArrays' | 'concatArrays'
 
 const options: {[k in MergeOptionType]: deepmerge.Options} = {
   replaceArrays: {
-    arrayMerge: (destinationArray, sourceArray, options) => sourceArray
+    arrayMerge: (destinationArray, sourceArray, options) => toRaw(sourceArray)
   },
   concatArrays: {
     arrayMerge: (target, source, options) => target.concat(...source)
